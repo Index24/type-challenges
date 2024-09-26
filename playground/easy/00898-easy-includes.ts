2046700c4d10eff20aa2696966b0341be1095311
Arif Balaev <balaev.arif@gmail.com>
2024-09-26 12:27:06 +0300

    feat: includes type

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -18,7 +18,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly unknown[], U> =
+  T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true ? true : Includes<Rest, U>
+    : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
